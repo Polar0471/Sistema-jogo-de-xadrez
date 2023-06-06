733344012218fc07886c5b81e247f15c6fec038a
Polar0471 <86422993+Polar0471@users.noreply.github.com>
2023-06-06 19:04:06 -0300

    camada de xadrez e exibindo o tabuleiro

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,12 @@
 package src.application;
 
-import src.application.boardgame.Board;
+import src.chess.ChessMatch;
 
 class Main {
     public static void main(String[] args) {
         
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
