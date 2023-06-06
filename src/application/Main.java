3a351d93b48427737f29f25f2ec17dcf2ebf9da9
Polar0471 <86422993+Polar0471@users.noreply.github.com>
2023-06-06 18:19:18 -0300

    criando a classe Position.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,12 @@
 package src.application;
 
+import src.application.boardgame.Position;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Position position = new Position(3, 5);
+        System.out.println(position);
+
     }
 }
