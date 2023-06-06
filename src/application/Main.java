0dfc5871d6e5fc895ddafe745d51fc315d46247e
Polar0471 <86422993+Polar0471@users.noreply.github.com>
2023-06-06 18:34:07 -0300

    começando a implementar Board e Piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,11 @@
 package src.application;
 
-import src.application.boardgame.Position;
+import src.application.boardgame.Board;
 
 class Main {
     public static void main(String[] args) {
         
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        Board board = new Board(8, 8);
 
     }
 }
---
+++ src/application/boardgame/Piece.java
@@ -0,0 +1,19 @@
+package src.application.boardgame;
+
+public class Piece {
+    
+    // a posição está como private pois não quero que ela fique visível na camada de xadrez. Ela é uma posição simples de matriz e não de xadrez
+    protected Position position;
+    private Board board;
+
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    // será acessado somente pelo pacote e pelas subclasses de Piece
+    protected Board getBoard() {
+        return this.board;
+    }
+
+}
