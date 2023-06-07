199f4e9880b92f0c924253cbcd2f66a4fce2bd40
Polar0471 <86422993+Polar0471@users.noreply.github.com>
2023-06-07 20:32:54 -0300

    Criando ChessException e ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package src.chess;
+
+public class ChessException extends RuntimeException {
+    private static final long seriealVersionUID = 1L;
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+    
+}
