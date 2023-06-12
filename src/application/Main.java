4b2f6a29ae5415e9af59ae7cb66aaee7c00ce264
Polar0471 <86422993+Polar0471@users.noreply.github.com>
2023-06-12 12:50:44 -0300

    Criando lógica para mover as peças

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,29 @@
 package src.application;
 
+import java.util.Scanner;
+
 import src.chess.ChessMatch;
+import src.chess.ChessPiece;
+import src.chess.ChessPosition;
 
 class Main {
     public static void main(String[] args) {
         
+        Scanner entrada = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(entrada);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(entrada);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
 
     }
 }
