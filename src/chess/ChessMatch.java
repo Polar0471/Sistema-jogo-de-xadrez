3e1abd5c679fb7367038aa034106e0292cac24ef
Polar0471 <86422993+Polar0471@users.noreply.github.com>
2023-06-15 11:25:33 -0300

    Criando a peça Bispo

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import src.boardgame.Board;
 import src.boardgame.Piece;
 import src.boardgame.Position;
+import src.chess.pieces.Bishop;
 import src.chess.pieces.King;
 import src.chess.pieces.Pawn;
 import src.chess.pieces.Rook;
@@ -195,7 +196,9 @@ public class ChessMatch {
 
     private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -207,7 +210,9 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
