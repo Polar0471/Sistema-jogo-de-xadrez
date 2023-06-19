78fb79ed8121e23b041454c91eb7a939f9d4fa96
Polar0471 <86422993+Polar0471@users.noreply.github.com>
2023-06-19 13:11:52 -0300

    Atualizando o movimento especial de Promoção

--- src/application/Main.java
+++ src/application/Main.java
@@ -41,7 +41,11 @@ class Main {
 
                 if (chessMatch.getPromoted() != null) {
                     System.out.print("Enter piece for promotion (B/N/R/Q): ");
-                    String type = entrada.nextLine();
+                    String type = entrada.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                        System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = entrada.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
             }
